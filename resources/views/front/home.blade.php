cd65b98b483548d2a2514661dfd0d41ba26ff82a
Marc Geurts <marc.geurts@scarlet.be>
2024-01-21 20:25:00 +0100

    Installation

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -11,7 +11,7 @@
                 <x-authentication-card-logo />
             </div>
 
-            <div class="w-full sm:max-w-5xl mt-6 p-6 bg-white shadow-md overflow-hidden rounded prose">
+            <div class="w-full sm:max-w-5xl mt-6 p-4 bg-white shadow-md overflow-hidden rounded prose">
                 <h1>Welcome ...</h1>
                 <x-hr.narrow class="!my-2" />
 
@@ -31,39 +31,33 @@
                                 <th>E-mail</th>
                                 <th>Password</th>
                                 <th>Purpose</th>
-                                <th>Role</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
                                 <td>administrator@genealogy.test</td>
                                 <td>password</td>
-                                <td>to view team BRITISH ROYALS as owner</td>
-                                <td>Administrator</td>
+                                <td>to view team BRITISH ROYALS as <b>owner</b></td>
                             </tr>
                             <tr>
                                 <td>manager@genealogy.test</td>
                                 <td>password</td>
-                                <td>to view team BRITISH ROYALS as manager</td>
-                                <td>Manager</td>
+                                <td>to view team BRITISH ROYALS as <b>manager</b></td>
                             </tr>
                             <tr>
                                 <td>editor@genealogy.test</td>
                                 <td>password</td>
-                                <td>to view team BRITISH ROYALS as editor</td>
-                                <td>Editor</td>
+                                <td>to view team BRITISH ROYALS as <b>editor</b></td>
                             </tr>
                             <tr>
                                 <td>member_1@genealogy.test</td>
                                 <td>password</td>
-                                <td>to view team BRITISH ROYALS as normal member</td>
-                                <td>Member</td>
+                                <td>to view team BRITISH ROYALS as normal <b>>member</b></td>
                             </tr>
                             <tr>
                                 <td>developer@genealogy.test</td>
                                 <td>password</td>
-                                <td>to view options reserved for developer, like offcanvas menu</td>
-                                <td>Developer</td>
+                                <td>to view options reserved for a <b>developer</b>, like the offcanvas menu</td>
                             </tr>
                         </tbody>
                     </table>
