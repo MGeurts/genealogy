cab8604050716a3e642116175925f67a0b98495a
Marc Geurts <marc.geurts@scarlet.be>
2024-01-30 09:28:14 +0100

    Installation

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,6 +1,12 @@
+@props(['logo' => '', 'header' => ''])
+
 <div class="w-full min-h-192 flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
     <div>
         {{ $logo }}
+    </div>
+
+    <div class="dark:text-gray-200">
+        {{ $header }}
     </div>
 
     <div class="w-full sm:max-w-xl mt-6 px-6 py-4 bg-white shadow-md overflow-hidden xs:rounded">
