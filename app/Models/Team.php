3318f00e1511ef84f2ea9d7508aa07bdec5c8f4d
Marc Geurts <marc.geurts@scarlet.be>
2024-02-03 15:17:40 +0100

    Implemented team summary for developer

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Laravel\Jetstream\Events\TeamCreated;
 use Laravel\Jetstream\Events\TeamDeleted;
 use Laravel\Jetstream\Events\TeamUpdated;
 use Laravel\Jetstream\Team as JetstreamTeam;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Team extends JetstreamTeam
 {
@@ -42,4 +43,19 @@ class Team extends JetstreamTeam
         'updated' => TeamUpdated::class,
         'deleted' => TeamDeleted::class,
     ];
+
+    /* -------------------------------------------------------------------------------------------- */
+    // Relations
+    /* -------------------------------------------------------------------------------------------- */
+    /* OK : returns ALL PERSON (n Person) */
+    public function persons(): HasMany
+    {
+        return $this->hasMany(Person::class);
+    }
+
+    /* OK : returns ALL COUPLE (n Couple) */
+    public function couples(): HasMany
+    {
+        return $this->hasMany(Couple::class);
+    }
 }
