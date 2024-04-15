1f0025658177b691b9f5ca72b3f45328c9fbc8c8
kreaweb.be <webmaster@kreaweb.be>
2024-04-15 09:51:07 +0200

    Installation

--- config/debugbar.php
+++ config/debugbar.php
@@ -14,7 +14,7 @@ return [
      |
      */
 
-    'enabled' => env('DEBUGBAR_ENABLED', false),
+    'enabled' => env('DEBUGBAR_ENABLED', null),
     'except' => [
         'telescope*',
         'horizon*',
