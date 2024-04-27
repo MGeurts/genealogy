524ea2c5f76f4626979a226fb93195f9ca8369d1
kreaweb.be <webmaster@kreaweb.be>
2024-04-27 23:02:08 +0200

    Show team in person heading if developer

--- resources/views/livewire/people/heading.blade.php
+++ resources/views/livewire/people/heading.blade.php
@@ -1,7 +1,11 @@
 <div class="p-3 pb-0 flex flex-col justify-end rounded dark:text-neutral-200 bg-white dark:bg-neutral-700 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
     <div class="flex flex-wrap">
         <div class="flex-grow max-w-full flex-1 text-lg font-medium">
-            {{ $person->name }}
+            <div>{{ $person->name }}</div>
+
+            @if (config('app.god_mode') && auth()->user()->is_developer)
+                <div class="text-info-500">{{ $person->team->name }}</div>
+            @endif
         </div>
 
         <div class="flex-grow max-w-full flex-1 text-end">
