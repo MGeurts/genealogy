bd4da33a31347d226a7ac8e5438dcfc1bd983183
kreaweb.be <webmaster@kreaweb.be>
2024-04-28 16:51:23 +0200

    Userlog factory

--- app/Models/Userlog.php
+++ app/Models/Userlog.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Carbon;
 
 class Userlog extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'user_id',
         'country_name',
---
+++ database/factories/UserlogFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Userlog>
+ */
+class UserlogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id'      => User::factory(),
+            'country_name' => $this->faker->country(),
+            'country_code' => $this->faker->countryCode(),
+            'created_at'   => $this->faker->dateTimeBetween('-2 year', '-1 day'),
+        ];
+    }
+}
