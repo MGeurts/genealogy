da9407d73e8ca5bd88360c5d159c8dc0fc627344
kreaweb.be <webmaster@kreaweb.be>
2024-05-26 22:47:42 +0200

    Improve layout: center markdown pages

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -9,14 +9,16 @@
         </h2>
     </x-slot>
 
-    <div class="pb-10 dark:text-neutral-200">
-        <div class="flex flex-col items-center pt-6 sm:pt-0">
-            <div>
-                <x-authentication-card-logo />
-            </div>
+    <div class="w-full py-5 space-y-5">
+        <div class="pb-10 dark:text-neutral-200">
+            <div class="flex flex-col items-center pt-6 sm:pt-0">
+                <div>
+                    <x-authentication-card-logo />
+                </div>
 
-            <div class="w-full sm:max-w-5xl mt-6 p-6 bg-white shadow-md overflow-hidden rounded prose">
-                {!! $terms !!}
+                <div class="w-full sm:max-w-5xl mt-6 p-6 bg-white shadow-md overflow-hidden rounded prose">
+                    {!! $terms !!}
+                </div>
             </div>
         </div>
     </div>
