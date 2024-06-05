c9b3ee8d59532b42bd5a957c3875f8b9ca7e7d7b
kreaweb.be <webmaster@kreaweb.be>
2024-06-05 16:21:51 +0200

    Removed indents from email template

--- resources/views/emails/team-invitation.blade.php
+++ resources/views/emails/team-invitation.blade.php
@@ -1,22 +1,22 @@
 @component('mail::message')
-    {{ __('team.been_invited', ['team' => $invitation->team->name]) }}
+{{ __('team.been_invited', ['team' => $invitation->team->name]) }}
 
-    @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::registration()))
-        {{ __('team.no_account') }}
+@if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::registration()))
+{{ __('team.no_account') }}
 
-        @component('mail::button', ['url' => route('register')])
-            {{ __('team.create_account') }}
-        @endcomponent
+@component('mail::button', ['url' => route('register')])
+{{ __('team.create_account') }}
+@endcomponent
 
-        {{ __('team.may_accept_already_account') }}
-    @else
-        {{ __('team.may_accept') }}
-    @endif
+{{ __('team.may_accept_already_account') }}
+@else
+{{ __('team.may_accept') }}
+@endif
 
 
-    @component('mail::button', ['url' => $acceptUrl])
-        {{ __('team.accept_invitation') }}
-    @endcomponent
+@component('mail::button', ['url' => $acceptUrl])
+{{ __('team.accept_invitation') }}
+@endcomponent
 
-    {{ __('team.discard') }}
+{{ __('team.discard') }}
 @endcomponent
