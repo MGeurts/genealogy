dc6b3dc327afe2c1891ca14f5b3d46163890715e
kreaweb.be <webmaster@kreaweb.be>
2024-06-14 10:03:00 +0200

    Installation

--- app/Livewire/Developer/PeopleLog.php
+++ app/Livewire/Developer/PeopleLog.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Livewire\Developer;
 
-use Livewire\Component;
 use Illuminate\Support\Facades\DB;
+use Livewire\Component;
 
 class PeopleLog extends Component
 {
@@ -14,12 +14,12 @@ class PeopleLog extends Component
     // -----------------------------------------------------------------------
     public function mount()
     {
-        $this->logs = collect(DB::select("
+        $this->logs = collect(DB::select('
             SELECT `activity_log`.`id`, `activity_log`.`event`, `activity_log`.`subject_type`, `activity_log`.`subject_id`, `activity_log`.`causer_id`, `activity_log`.`properties` , `activity_log`.`created_at`, `users`.`firstname`, `users`.`surname`
             FROM activity_log LEFT JOIN users ON (`activity_log`.`causer_id` = `users`.`id`)
             ORDER BY created_at DESC 
             LIMIT 30;
-        "));
+        '));
     }
 
     // ------------------------------------------------------------------------------
@@ -27,5 +27,4 @@ class PeopleLog extends Component
     {
         return view('livewire.developer.peoplelog');
     }
-
 }
