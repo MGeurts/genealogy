b15cd803407ee174644babd10a8efeb1b3f204ef
kreaweb.be <webmaster@kreaweb.be>
2024-06-22 09:58:55 +0200

    Test: GEDCOM Parsing

--- resources/views/back/developer/test.blade.php
+++ resources/views/back/developer/test.blade.php
@@ -10,6 +10,19 @@
     <div class="grow max-w-5xl overflow-x-auto py-5 dark:text-neutral-200">
 
         <p>Use this page to test components ...</p>
+        <br />
+
+        @php
+            $gedcom = App\Person::parse('./gedcom/royals.ged');
+
+            foreach ($gedcom['INDI'] as $person) {
+                $person = new App\Person($person, $gedcom);
+                echo $person->surname() . ', ' . $person->forename() . ' ' . $person->years() . "\n<br/>";
+                // echo $person->link() . "\n";
+                // echo $person->name() . ' father is ' . $person->father->()->name();
+                // echo $person->name() . ' maternal grandfather is ' . $gedcom['INDI']['I1']->mother->()->father()->name();
+            }
+        @endphp
 
     </div>
 </x-app-layout>
