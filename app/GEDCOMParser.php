05b52f6585da97555136c9240a8da32749dfdc91
kreaweb.be <webmaster@kreaweb.be>
2024-06-25 17:15:29 +0200

    Testing: GEDCOM Parser

--- app/GEDCOMParser.php
+++ app/GEDCOMParser.php
@@ -5,47 +5,53 @@ namespace App;
 class GedcomParser
 {
     protected $individuals = [];
+
     protected $families = [];
 
     public function parse($filePath)
     {
-        if (!file_exists($filePath)) {
-            echo "File not found: $filePath<br/>";
-            return;
-        }
-
-        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        $lines         = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         $currentEntity = null;
-        $currentType = null;
+        $currentType   = null;
 
         foreach ($lines as $line) {
-            $level = (int)substr($line, 0, 1);
+            $level = (int) substr($line, 0, 1);
             $parts = explode(' ', trim(substr($line, 2)), 2);
-            $tag = $parts[0];
-            $data = isset($parts[1]) ? $parts[1] : null;
+            $tag   = $parts[0];
+            $data  = isset($parts[1]) ? $parts[1] : null;
 
             if ($level === 0) {
                 if (strpos($tag, '@') !== false && strpos($data, 'INDI') !== false) {
-                    $currentType = 'individual';
-                    $currentEntity = $tag;
+                    $currentType                       = 'individual';
+                    $currentEntity                     = $tag;
                     $this->individuals[$currentEntity] = [];
                 } elseif (strpos($tag, '@') !== false && strpos($data, 'FAM') !== false) {
-                    $currentType = 'family';
-                    $currentEntity = $tag;
+                    $currentType                    = 'family';
+                    $currentEntity                  = $tag;
                     $this->families[$currentEntity] = [];
                 }
             } else {
                 if ($currentType === 'individual') {
-                    if (!isset($this->individuals[$currentEntity][$tag])) {
-                        $this->individuals[$currentEntity][$tag] = $data;
+                    if ($tag === 'CONC' || $tag === 'CONT') {
+                        $lastIndex = count($this->individuals[$currentEntity]) - 1;
+                        if ($tag === 'CONC') {
+                            $this->individuals[$currentEntity][$lastIndex]['data'] .= $data;
+                        } elseif ($tag === 'CONT') {
+                            $this->individuals[$currentEntity][$lastIndex]['data'] .= "\n" . $data;
+                        }
                     } else {
-                        $this->individuals[$currentEntity][$tag] .= "<br/>" . $data;
+                        $this->individuals[$currentEntity][] = ['level' => $level, 'tag' => $tag, 'data' => $data];
                     }
                 } elseif ($currentType === 'family') {
-                    if (!isset($this->families[$currentEntity][$tag])) {
-                        $this->families[$currentEntity][$tag] = $data;
+                    if ($tag === 'CONC' || $tag === 'CONT') {
+                        $lastIndex = count($this->families[$currentEntity]) - 1;
+                        if ($tag === 'CONC') {
+                            $this->families[$currentEntity][$lastIndex]['data'] .= $data;
+                        } elseif ($tag === 'CONT') {
+                            $this->families[$currentEntity][$lastIndex]['data'] .= "\n" . $data;
+                        }
                     } else {
-                        $this->families[$currentEntity][$tag] .= "<br/>" . $data;
+                        $this->families[$currentEntity][] = ['level' => $level, 'tag' => $tag, 'data' => $data];
                     }
                 }
             }
@@ -65,26 +71,33 @@ class GedcomParser
     public function outputIndividuals()
     {
         foreach ($this->individuals as $id => $tags) {
-            echo "-------------------- INDIVIDUAL " . $id . " --------------------<br/>";
+            echo '<h2>-------------------- INDIVIDUAL ' . $id . ' --------------------</h2>';
 
-            foreach ($tags as $tag => $data) {
-                echo "  $tag: $data<br/>";
-            }
-            echo "<br/>";
+            echo '<div>';
+            $this->outputTags($tags);
+            echo '</div><br/>';
         }
     }
 
     public function outputFamilies()
     {
         foreach ($this->families as $id => $tags) {
-            echo "-------------------- FAMILY " . $id . " --------------------<br/>";
+            echo '<h2>-------------------- FAMILY ' . $id . ' --------------------</h2>';
 
-            foreach ($tags as $tag => $data) {
-                if ($tag === 'HUSB' || $tag === 'WIFE' || $tag === 'CHIL') {
-                    echo "  $tag: $data<br/>";
+            foreach ($tags as $tag) {
+                if ($tag['tag'] === 'HUSB' || $tag['tag'] === 'WIFE' || $tag['tag'] === 'CHIL') {
+                    echo "&emsp;{$tag['tag']}&nbsp;: {$tag['data']}<br>";
                 }
             }
-            echo "<br/>";
+        }
+    }
+
+    protected function outputTags($tags, $baseLevel = 0)
+    {
+        foreach ($tags as $tag) {
+            $indentation = str_repeat('&emsp;', $tag['level'] - $baseLevel);
+
+            echo "{$indentation}{$tag['tag']}&nbsp;: {$tag['data']}<br>";
         }
     }
 }
