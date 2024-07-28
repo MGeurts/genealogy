555625b5c949288393e85ed1f8c5b46aaa2a18bb
kreaweb.be <webmaster@kreaweb.be>
2024-07-28 18:32:26 +0200

    Added badges in Team overview

--- resources/views/livewire/team.blade.php
+++ resources/views/livewire/team.blade.php
@@ -2,10 +2,21 @@
     <table class="min-w-full text-left whitespace-nowrap">
         <thead class="tracking-wider uppercase border-t border-b-2 dark:border-neutral-600 bg-neutral-50 dark:bg-neutral-800">
             <tr>
-                <th scope="col" class="p-2 border-x dark:border-neutral-600">{{ __('team.team') }}</th>
-                <th scope="col" class="p-2 border-x dark:border-neutral-600">{{ __('team.users') }}</th>
-                <th scope="col" class="p-2 border-x dark:border-neutral-600">{{ __('team.persons') }}</th>
-                <th scope="col" class="p-2 border-x dark:border-neutral-600">{{ __('team.couples') }}</th>
+                <th scope="col" class="p-2 border-x dark:border-neutral-600">
+                    {{ __('team.team') }}
+                </th>
+                <th scope="col" class="p-2 border-x dark:border-neutral-600">
+                    {{ __('team.users') }}
+                    <x-ts-badge color="emerald" text="{{ count($user->currentTeam->users) }}" />
+                </th>
+                <th scope="col" class="p-2 border-x dark:border-neutral-600">
+                    {{ __('team.persons') }}
+                    <x-ts-badge color="emerald" text="{{ count($user->currentTeam->persons) }}" />
+                    </th>
+                <th scope="col" class="p-2 border-x dark:border-neutral-600">
+                    {{ __('team.couples') }}
+                    <x-ts-badge color="emerald" text="{{ count($user->currentTeam->couples) }}" />
+                </th>
             </tr>
         </thead>
 
