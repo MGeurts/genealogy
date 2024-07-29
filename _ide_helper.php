161794306ea538f76f93560fe1b58e75c7cf3be2
kreaweb.be <webmaster@kreaweb.be>
2024-07-29 10:54:50 +0200

    Installation

--- _ide_helper.php
+++ _ide_helper.php
@@ -26986,6 +26986,11 @@ namespace  {
     }
 
 
+namespace Facades\Livewire\Features\SupportFileUploads {
+    /**
+     * @mixin \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl     */
+    class GenerateSignedUploadUrl extends \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl {}
+}
 
 
 
