a3d56119d98dfbb04bea965f122b168d42ef8e2a
kreaweb.be <webmaster@kreaweb.be>
2024-09-21 14:55:41 +0200

    Version bump

--- _ide_helper.php
+++ _ide_helper.php
@@ -27470,6 +27470,11 @@ namespace  {
     }
 
 
+namespace Facades\Livewire\Features\SupportFileUploads {
+    /**
+     * @mixin \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl     */
+    class GenerateSignedUploadUrl extends \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl {}
+}
 
 
 
