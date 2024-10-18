963e965f1e81b7fea4fd2d828263ce689b4413a8
diepdo1810 <93646638+diepdo1810@users.noreply.github.com>
2024-10-18 14:27:12 +0700

    feat: add Vietnamese language suppor (#22)
    update: format validation
    
    update: reafactor code
    
    Co-authored-by: diepdp <diepdp@kaiyouit.com>

---
+++ lang/vi/metadata.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'address' => 'Địa chỉ',
+
+    'location_name' => 'Tên địa điểm',
+    'latitude'      => 'Vĩ độ',
+    'longitude'     => 'Kinh độ',
+];
---
+++ lang/vi/team.php
@@ -0,0 +1,66 @@
+<?php
+
+return [
+    'all_teams'     => 'Tất cả các đội',
+    'cancel'        => 'Hủy',
+    'couples'       => 'Cặp đôi',
+    'create'        => 'Tạo đội',
+    'delete'        => 'Xóa đội',
+    'description'   => 'Mô tả',
+    'id'            => 'ID',
+    'manage'        => 'Quản lý đội',
+    'name'          => 'Tên',
+    'owner'         => 'Chủ sở hữu',
+    'persons'       => 'Người',
+    'settings'      => 'Cài đặt đội',
+    'switch'        => 'Chuyển đổi đội',
+    'team'          => 'Đội',
+    'team_personal' => 'Đội cá nhân',
+    'teams'         => 'Các đội',
+    'users'         => 'Người dùng',
+
+    // Messages
+    'team_details'    => 'Chi tiết đội',
+    'team_create_new' => 'Tạo một đội mới để hợp tác với người khác.',
+
+    'team_name'               => 'Tên đội',
+    'team_information'        => 'Thông tin về tên và chủ sở hữu đội.',
+    'team_add_member'         => 'Thêm thành viên đội',
+    'team_add_member_message' => 'Thêm một thành viên mới vào đội của bạn, cho phép họ hợp tác với bạn.',
+    'team_provide_email'      => 'Vui lòng cung cấp địa chỉ email của người dùng mới mà bạn muốn thêm vào đội này.',
+    'team_provide_role'       => 'Sau đó, hãy chọn vai trò cho người dùng mới.',
+    'email'                   => 'Email',
+    'role'                    => 'Vai trò',
+
+    'team_pending'         => 'Lời mời tham gia đội đang chờ',
+    'team_pending_message' => 'Những người này đã được mời tham gia đội của bạn và đã nhận được email mời. Họ có thể tham gia đội bằng cách chấp nhận lời mời qua email.',
+
+    'change_role'          => 'Thay đổi vai trò',
+    'team_members'         => 'Thành viên đội',
+    'team_members_message' => 'Tất cả những người thuộc về đội này.',
+    'manage_role'          => 'Quản lý vai trò',
+    'leave'                => 'Rời khỏi',
+    'leave_team'           => 'Rời khỏi đội',
+    'leave_team_sure'      => 'Bạn có chắc chắn muốn rời khỏi đội này không?',
+    'remove'               => 'Xóa',
+    'remove_member'        => 'Xóa thành viên đội',
+    'remove_member_sure'   => 'Bạn có chắc chắn muốn xóa người này khỏi đội không?',
+    'send_email'           => 'Gửi email',
+
+    'user_not_found'       => 'Chúng tôi không thể tìm thấy người dùng đã đăng ký với địa chỉ email này.',
+    'user_already_in_team' => 'Người dùng này đã thuộc về đội.',
+    'user_already_invited' => 'Người dùng này đã được mời tham gia đội.',
+    'user_not_leave'       => 'Bạn không thể rời khỏi đội mà bạn đã tạo.',
+
+    'delete_team_meassage' => 'Xóa đội vĩnh viễn.',
+    'delete_team_text'     => 'Khi một đội bị xóa, tất cả tài nguyên và dữ liệu của nó sẽ bị xóa vĩnh viễn. Trước khi xóa đội này, vui lòng tải xuống bất kỳ dữ liệu hoặc thông tin nào liên quan đến đội mà bạn muốn giữ lại.',
+    'delete_team_sure'     => 'Bạn có chắc chắn muốn xóa đội này không? Khi một đội bị xóa, tất cả tài nguyên và dữ liệu của nó sẽ bị xóa vĩnh viễn.',
+
+    'been_invited'               => 'Bạn đã được mời tham gia đội :team!',
+    'no_account'                 => 'Nếu bạn chưa có tài khoản, bạn có thể tạo một tài khoản bằng cách nhấp vào nút bên dưới. Sau khi tạo tài khoản, bạn có thể nhấp vào nút chấp nhận lời mời trong email này để chấp nhận lời mời tham gia đội:',
+    'create_account'             => 'Tạo tài khoản',
+    'may_accept_already_account' => 'Nếu bạn đã có tài khoản, bạn có thể chấp nhận lời mời này bằng cách nhấp vào nút bên dưới:',
+    'may_accept'                 => 'Bạn có thể chấp nhận lời mời này bằng cách nhấp vào nút bên dưới:',
+    'accept_invitation'          => 'Chấp nhận lời mời',
+    'discard'                    => 'Nếu bạn không mong đợi nhận được lời mời tham gia đội này, bạn có thể bỏ qua email này.',
+];
