eb6ec7f3695552a1460f2bf1089fa758d487dfb6
MGeurts <5404867+MGeurts@users.noreply.github.com>
2024-10-18 09:11:24 +0200

    Updated composer.json and package.json

--- _ide_helper.php
+++ _ide_helper.php
@@ -5,7 +5,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 11.28.0.
+ * Generated for Laravel 11.28.1.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
@@ -838,7 +838,7 @@ namespace Illuminate\Support\Facades {
                     /**
          * Get the service providers that have been loaded.
          *
-         * @return array<string, boolean> 
+         * @return array<string, bool> 
          * @static 
          */        public static function getLoadedProviders()
         {
