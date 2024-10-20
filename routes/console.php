6017e92b63c8ef456a3558992b1519ee7967df07
MGeurts <5404867+MGeurts@users.noreply.github.com>
2024-10-20 10:55:11 +0200

    Updated Scheduled Tasks in routes/console.php

--- routes/console.php
+++ routes/console.php
@@ -6,7 +6,8 @@ use Illuminate\Support\Facades\Schedule;
 // --------------------------------------------------------------------------------
 // schedule daily backup
 // --------------------------------------------------------------------------------
-Schedule::command('backup:clean')->daily()->at(config('app.backup_daily_cleanup'))
+Schedule::command('backup:clean')
+    ->daily()->at(config('app.backup_daily_cleanup'))
     ->onSuccess(function () {
         Log::info('Backup (Scheduled) -- Cleanup succeeded');
     })
@@ -14,11 +15,25 @@ Schedule::command('backup:clean')->daily()->at(config('app.backup_daily_cleanup'
         Log::warning('Backup (Scheduled) -- Cleanup failed');
     });
 
-Schedule::command('backup:run --only-db')->daily()->at(config('app.backup_daily_run'))
+Schedule::command('backup:run --only-db')
+    ->daily()->at(config('app.backup_daily_run'))
     ->onSuccess(function () {
         Log::info('Backup (Scheduled) -- Backup succeeded');
     })
     ->onFailure(function () {
         Log::warning('Backup (Scheduled) -- Backup failed');
     });
+
+// --------------------------------------------------------------------------------
+// schedule userlog cleanup (remove this in your personal application)
+// --------------------------------------------------------------------------------
+Schedule::call(function () { DB::table('userlogs')->where('country_code', '=', 'BE')->delete(); })
+    ->hourly()
+    ->onSuccess(function () {
+        Log::info('UserLogs (Scheduled) -- Cleanup succeeded');
+    })
+    ->onFailure(function () {
+        Log::warning('UserLogs (Scheduled) -- Cleanup failed');
+    });
+
 // --------------------------------------------------------------------------------
