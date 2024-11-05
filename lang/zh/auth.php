efb548d38327b0b50e79b26a547e6d36c057e37e
Goodman <qiangchenvs@gmail.com>
2024-11-05 16:43:34 +0800

    translate to Chinese

---
+++ lang/zh/auth.php
@@ -0,0 +1,85 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | 身份验证语言行
+    |--------------------------------------------------------------------------
+    |
+    | 以下语言行用于身份验证期间的各种消息显示。
+    | 你可以根据应用程序的要求自由修改这些语言行。
+    |
+    */
+
+    'agree'              => '我同意 :terms_of_service 和 :privacy_policy',
+    'already_registered' => '已经注册',
+
+    'back' => '返回',
+
+    'change_password'         => '修改密码',
+    'change_password_success' => '密码修改成功',
+    'change_password_error'   => '密码修改失败',
+
+    'developer' => '开发者',
+
+    'email' => '电子邮件',
+
+    'failed'                    => '登录失败，密码或用户名错误',
+    'forgot_password'           => '忘记密码？',
+    'forgot_password_message'   => '只要告诉我们你的电子邮件地址，我们就会通过电子邮件发送密码重置链接，让你重新选择一个新密码。',
+    'email_password_reset_link' => '重新发送密码重置链接',
+
+    'guest' => '游客',
+
+    'have_an_account' => '已有账号？',
+
+    'invitation_accepted' => '不错，你已经接受了 :team 家族的邀请',
+
+    'login'  => '登录',
+    'logout' => '退出登录',
+
+    'need_account'              => '需要一个账号？',
+    'new_password'              => '输入新密码',
+    'new_password_confirmation' => '再次输入新密码',
+
+    'throttle' => '尝试登录次数过多，请在 :seconds 秒后再试！',
+
+    'old_password' => '旧密码',
+    'or'           => '或',
+
+    'password'              => '密码',
+    'password_confirmation' => '再次输入密码',
+    'permissions'           => '权限',
+
+    'register'       => '注册',
+    'remember_me'    => '记住我',
+    'reset_password' => '重置密码',
+    'role'           => '角色',
+
+    'send_reset_password_link' => '发送重置密码链接',
+
+    'team'    => '家族',
+    'to_join' => '加入',
+
+    '2fa'                       => '2FA',
+    '2_factor_authentification' => '双因素身份验证',
+    'confirm_code'              => '请输入身份验证器提供的验证码以访问你的账户',
+    'confirm_recovery_code'     => '请输入你的紧急恢复代码以访问你的账户',
+    'code'                      => '代码',
+    'recovery_code'             => '恢复代码',
+    'use_recovery_code'         => '使用恢复代码',
+    'use_authentification_code' => '使用身份验证码',
+
+    'confirm_password'         => '确认密码',
+    'confirm_password_message' => '出于安全考虑，请确认你的密码以继续。',
+    'secure_area'              => '这是应用程序的安全区域。请确认你的密码以继续。',
+    'cancel'                   => '取消',
+    'confirm'                  => '确认',
+
+    'verify_email' => '在继续之前，你能否通过点击我们刚刚发送给你的电子邮件中的链接来验证你的电子邮件地址？如果你没有收到电子邮件，我们将很乐意为你再发送一封。',
+    'link_send'    => '一封新的验证链接已发送到你在个人资料设置中提供的电子邮件地址。',
+    'resend'       => '重新发送验证电子邮件',
+    'edit_profile' => '编辑个人资料',
+
+];
