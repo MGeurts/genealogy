860bcf8144bde9bc724cc3b1f1ecdebf5fd48182
MGeurts <5404867+MGeurts@users.noreply.github.com>
2024-11-05 15:17:44 +0100

    Improved Country Lookup

--- app/Countries.php
+++ app/Countries.php
@@ -13,10 +13,22 @@ class Countries
     // -----------------------------------------------------------------------
     public function __construct(string $locale = 'en')
     {
+        $convert_locales_to_countries = [
+            'de' => 'de',
+            'en' => 'en',
+            'es' => 'es',
+            'fr' => 'fr',
+            'nl' => 'nl',
+            'pt' => 'pt',
+            'zh_cn' => 'zh',
+        ];
+
+        $country = $convert_locales_to_countries[$locale] ?? 'en';
+
         $path = base_path('vendor/stefangabos/world_countries/data/countries/');
 
-        if (file_exists($path . $locale) && is_dir($path . $locale)) {
-            $this->countries = collect(require $path . $locale . '/countries.php');
+        if (file_exists($path . $country) && is_dir($path . $country)) {
+            $this->countries = collect(require $path . $country . '/countries.php');
         } else {
             $this->countries = collect(require $path . 'en/countries.php');
         }
