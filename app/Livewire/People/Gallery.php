95e9af9f56fedaabcd720b30fc6aebb1553090e6
MGeurts <5404867+MGeurts@users.noreply.github.com>
2024-11-08 18:22:48 +0100

    BUGFIX: Optimized Gallery Livewire component

--- app/Livewire/People/Gallery.php
+++ app/Livewire/People/Gallery.php
@@ -27,36 +27,36 @@ class Gallery extends Component
     // ------------------------------------------------------------------------------
     public function mount(): void
     {
-        $this->images = collect(File::glob(public_path() . '/storage/photos/' . $this->person->team_id . '/' . $this->person->id . '_*.webp'))
-            ->map(function ($p) {
-                return substr($p, strrpos($p, '/') + 1);
-            })->toArray();
+        // Fetch all the image filenames for the person
+        $path = public_path("storage/photos/{$this->person->team_id}/{$this->person->id}_*.webp");
 
-        if (count($this->images) > 0) {
-            if ($this->person->photo) {
-                $this->selected = array_search($this->person->photo, $this->images);
-            } else {
-                $this->selected = 0;
-            }
+        $this->images = collect(File::glob($path))
+            ->map(fn ($p) => basename($p)) // Extract filename
+            ->toArray();
+
+        // Set the primary image (if any) as selected
+        $this->selected = $this->getPrimaryImageIndex();
+    }
+
+    protected function getPrimaryImageIndex(): ?int
+    {
+        if (count($this->images) === 0) {
+            return null;
         }
+
+        return $this->person->photo
+            ? (int) array_search($this->person->photo, $this->images)
+            : 0;
     }
 
     public function previousImage(): void
     {
-        $this->selected--;
-
-        if ($this->selected < 0) {
-            $this->selected = count($this->images) - 1;
-        }
+        $this->selected = ($this->selected - 1 + count($this->images)) % count($this->images);
     }
 
     public function nextImage(): void
     {
-        $this->selected++;
-
-        if ($this->selected == count($this->images)) {
-            $this->selected = 0;
-        }
+        $this->selected = ($this->selected + 1) % count($this->images);
     }
 
     public function selectImage($index): void
