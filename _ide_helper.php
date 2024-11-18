3b0694dc0973f6100fc0b483971d64100b39893e
MGeurts <5404867+MGeurts@users.noreply.github.com>
2024-11-18 18:39:09 +0100

    Version bump

--- _ide_helper.php
+++ _ide_helper.php
@@ -27418,11 +27418,6 @@ namespace  {
     }
 
 
-namespace Facades\Livewire\Features\SupportFileUploads {
-    /**
-     * @mixin \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl     */
-    class GenerateSignedUploadUrl extends \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl {}
-}
 
 
 
