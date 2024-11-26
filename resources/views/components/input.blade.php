fe3f4f6643c7024074f2f34403404094180f3151
MGeurts <5404867+MGeurts@users.noreply.github.com>
2024-11-26 22:15:11 +0100

    Improved layout disabled input and textarea

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,7 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+    'class' => $disabled
+        ? 'block w-full rounded border-gray-300 bg-gray-100 text-gray-500 shadow-sm cursor-not-allowed'
+        : 'block w-full rounded border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 shadow-sm',
+]) !!}>
--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,5 +1,9 @@
 @props(['disabled' => false])
 
-<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'peer block min-h-[auto] w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded shadow-sm px-3 py-[0.32rem]']) !!}>
+<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+    'class' => $disabled
+        ? 'block w-full min-h-[auto] rounded border-gray-300 bg-gray-100 text-gray-500 shadow-sm px-3 py-[0.32rem] cursor-not-allowed peer'
+        : 'block w-full min-h-[auto] rounded border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 shadow-sm px-3 py-[0.32rem] peer',
+]) !!}>
     {{ $slot }}
 </textarea>
