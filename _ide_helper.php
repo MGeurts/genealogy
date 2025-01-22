e7befd1a7f87f0f214c9bf1e4406abe7dc61f099
MGeurts <5404867+MGeurts@users.noreply.github.com>
2025-01-22 09:13:43 +0100

    Version bump

--- _ide_helper.php
+++ _ide_helper.php
@@ -5,7 +5,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 11.38.2.
+ * Generated for Laravel 11.39.0.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
