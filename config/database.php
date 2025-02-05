f28e333bc989bb7e0935ce10b4302d867fd854a6
MGeurts <5404867+MGeurts@users.noreply.github.com>
2025-02-05 17:53:08 +0100

    Set Path to mysqldump.exe

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
             'dump' => [
-                'dump_binary_path' => 'C:/Program Files/MySQL/MySQL Server 9.1/bin/', // only the path, so without `mysqldump` or `pg_dump`
+                'dump_binary_path' => env('BACKUP_DUMP_PATH', null), // only the path, so without `mysqldump` or `pg_dump`
                 'use_single_transaction',
                 'timeout' => 60 * 5, // 5 minute timeout
                 // 'exclude_tables' => [],
