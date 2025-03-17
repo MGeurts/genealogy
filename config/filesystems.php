16465e28b48c3483b6ab0f250ec38fcd42356e13
MGeurts <5404867+MGeurts@users.noreply.github.com>
2025-03-17 14:20:04 +0100

    Cleanup config/filesystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -31,13 +31,35 @@ return [
     */
 
     'disks' => [
-
         'local' => [
             'driver' => 'local',
             'root'   => storage_path('app'),
             'throw'  => false,
         ],
 
+        'public' => [
+            'driver'     => 'local',
+            'root'       => storage_path('app/public'),
+            'url'        => env('APP_URL') . '/storage',
+            'visibility' => 'public',
+            'throw'      => false,
+        ],
+
+        's3' => [
+            'driver'                  => 's3',
+            'key'                     => env('AWS_ACCESS_KEY_ID'),
+            'secret'                  => env('AWS_SECRET_ACCESS_KEY'),
+            'region'                  => env('AWS_DEFAULT_REGION'),
+            'bucket'                  => env('AWS_BUCKET'),
+            'url'                     => env('AWS_URL'),
+            'endpoint'                => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw'                   => false,
+        ],
+
+        // --------------------------------------------------------------------------
+        // CUSTOM STORAGE
+        // --------------------------------------------------------------------------
         // backup disk
         env('BACKUP_DISK', 'backups') => [
             'driver' => 'local',
@@ -98,27 +120,7 @@ return [
             'visibility' => 'public',
             'throw'      => false,
         ],
-
-        'public' => [
-            'driver'     => 'local',
-            'root'       => storage_path('app/public'),
-            'url'        => env('APP_URL') . '/storage',
-            'visibility' => 'public',
-            'throw'      => false,
-        ],
-
-        's3' => [
-            'driver'                  => 's3',
-            'key'                     => env('AWS_ACCESS_KEY_ID'),
-            'secret'                  => env('AWS_SECRET_ACCESS_KEY'),
-            'region'                  => env('AWS_DEFAULT_REGION'),
-            'bucket'                  => env('AWS_BUCKET'),
-            'url'                     => env('AWS_URL'),
-            'endpoint'                => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw'                   => false,
-        ],
-
+        // --------------------------------------------------------------------------
     ],
 
     /*
