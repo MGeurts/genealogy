337fefc9210edd691042b6a42f24f38a8634c479
MGeurts <5404867+MGeurts@users.noreply.github.com>
2025-04-21 10:01:19 +0200

    Bugfix: Fix modal component backdrop z-index issue

--- resources/views/components/confirmation-modal.blade.php
+++ resources/views/components/confirmation-modal.blade.php
@@ -1,7 +1,7 @@
 @props(['id' => null, 'maxWidth' => null])
 
 <x-modal :id="$id" :maxWidth="$maxWidth" {{ $attributes }}>
-    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 rounded-t-sm">
         <div class="sm:flex sm:items-start">
             <div class="mx-auto shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                 <svg class="size-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -22,7 +22,7 @@
         </div>
     </div>
 
-    <div class="flex flex-row justify-end px-6 py-4 bg-gray-100 text-end">
+    <div class="flex flex-row justify-end px-6 py-4 bg-gray-100 text-end rounded-b-sm">
         {{ $footer }}
     </div>
 </x-modal>
