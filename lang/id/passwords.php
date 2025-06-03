0b020fc1258d9192737740dad61d30b405bb3867
MGeurts <5404867+MGeurts@users.noreply.github.com>
2025-06-03 10:23:39 +0200

    Pint

--- lang/id/passwords.php
+++ lang/id/passwords.php
@@ -19,7 +19,7 @@ return [
     'reset'         => 'Kata sandi Anda telah diatur ulang!',
     'sent'          => 'Kami telah mengirimkan tautan pengaturan ulang kata sandi Anda melalui email!',
     'token'         => 'Token pengaturan ulang kata sandi ini tidak valid.',
-    'user'          => "Kami tidak dapat menemukan pengguna dengan alamat email tersebut.",
+    'user'          => 'Kami tidak dapat menemukan pengguna dengan alamat email tersebut.',
     'old_password'  => 'Kata sandi Anda saat ini tidak cocok dengan kata sandi yang Anda berikan. Silakan coba lagi.',
     'same_password' => 'Kata sandi baru tidak boleh sama dengan kata sandi Anda saat ini. Silakan pilih kata sandi yang berbeda.',
 ];
