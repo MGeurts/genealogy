232af4862c741080be102cb4ed3e09f26c10895c
MGeurts <5404867+MGeurts@users.noreply.github.com>
2025-06-08 10:25:40 +0200

    Bugfix Descendants and Ancestors Tree

--- app/Livewire/People/Ancestors.php
+++ app/Livewire/People/Ancestors.php
@@ -20,16 +20,16 @@ final class Ancestors extends Component
 
     public int $count = 3;          // default, showing 3 levels (person & parents & grandparents)
 
-    public int $count_max = 128;    // maximum level depth, choose from listing below
+    public int $count_max = 128;    // maximum level depth, choose carefully from listing below
 
     // --------------------------------------------------------------------------------------------------------------------
     // REMARK : The maximum length of the comma separated sequence of all id's in the tree can NOT succeed 1024 characters!
-    //          So, when the id's are 3 digits (max        999), the maximum level depth is 1024 / (3 + 1) = 256 levels
-    //              when the id's are 4 digits (max      9.999), the maximum level depth is 1024 / (4 + 1) = 204 levels
-    //              when the id's are 5 digits (max     99.999), the maximum level depth is 1024 / (5 + 1) = 170 levels
-    //              when the id's are 6 digits (max    999.999), the maximum level depth is 1024 / (6 + 1) = 146 levels
-    //              when the id's are 7 digits (max  9.999.999), the maximum level depth is 1024 / (7 + 1) = 128 levels
-    //              when the id's are 8 digits (max 99.999.999), the maximum level depth is 1024 / (8 + 1) = 113 levels
+    //          So, when largest id is 3 digits (max        999), the maximum level depth is 1024 / (3 + 1) = 256 levels
+    //              when largest id is 4 digits (max      9.999), the maximum level depth is 1024 / (4 + 1) = 204 levels
+    //              when largest id is 5 digits (max     99.999), the maximum level depth is 1024 / (5 + 1) = 170 levels
+    //              when largest id is 6 digits (max    999.999), the maximum level depth is 1024 / (6 + 1) = 146 levels
+    //              when largest id is 7 digits (max  9.999.999), the maximum level depth is 1024 / (7 + 1) = 128 levels
+    //              when largest id is 8 digits (max 99.999.999), the maximum level depth is 1024 / (8 + 1) = 113 levels
     //              ...
     // --------------------------------------------------------------------------------------------------------------------
 
