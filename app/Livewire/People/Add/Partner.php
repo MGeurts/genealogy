9af76f566a452ed4b2a1372bff0146408d4bbcb9
Chinmay Purav <chinpurav@gmail.com>
2025-08-02 23:18:07 +0530

    fix: update image upload logic to use media collection method

--- app/Livewire/People/Add/Partner.php
+++ app/Livewire/People/Add/Partner.php
@@ -8,7 +8,6 @@ use App\Livewire\Forms\People\PersonForm;
 use App\Livewire\Traits\TrimStringsAndConvertEmptyStringsToNull;
 use App\Models\Couple;
 use App\Models\Person;
-use App\PersonPhotos;
 use App\Rules\DobValid;
 use App\Rules\YobValid;
 use Illuminate\Http\UploadedFile;
@@ -156,10 +155,13 @@ final class Partner extends Component
                     'team_id'   => $this->person->team_id,
                 ]);
 
-                if ($this->form->uploads) {
-                    $photos     = new PersonPhotos($newPartner);
-                    $savedCount = $photos->save($this->form->uploads);
+                foreach ($this->form->uploads as $upload) {
+                    $newPartner
+                        ->addMediaFromDisk($upload->getClientOriginalPath())
+                        ->toMediaCollection();
+                }
 
+                if ($savedCount = count($this->form->uploads)) {
                     $this->toast()->success(__('app.save'), trans_choice('person.photos_saved', $savedCount))->send();
                 }
 
