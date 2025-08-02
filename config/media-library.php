38a6498dc0b08f0d7daf4ba8f942695ae53741f3
Chinmay Purav <chinpurav@gmail.com>
2025-08-02 23:03:10 +0530

    fix: change default media disk to 'local'

--- config/media-library.php
+++ config/media-library.php
@@ -8,7 +8,7 @@ return [
      * The disk on which to store added files and derived images by default.
      * Choose one or more of the disks you've configured in config/filesystems.php.
      */
-    'disk_name' => env('MEDIA_DISK', 'files'),
+    'disk_name' => env('MEDIA_DISK', 'local'),
 
     /*
      * The maximum file size of an item in bytes.
