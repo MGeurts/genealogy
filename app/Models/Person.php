1cf0680ac6a1209c9c8ffb1caa2be6b5ff8ef347
Chinmay Purav <chinpurav@gmail.com>
2025-08-03 21:23:23 +0530

    optimize name accessor

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -19,6 +19,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 use Korridor\LaravelHasManyMerged\HasManyMerged;
 use Korridor\LaravelHasManyMerged\HasManyMergedRelation;
 use Override;
@@ -440,11 +441,12 @@ final class Person extends Model implements HasMedia
     /* -------------------------------------------------------------------------------------------- */
     protected function name(): Attribute
     {
-        return Attribute::make(get: function (): ?string {
-            $name = mb_trim("{$this->firstname} {$this->surname}");
-
-            return $name ?: null;
-        });
+        return Attribute::get(
+            fn () => Str::of($this->surname)
+                ->prepend($this->firstname, ' ')
+                ->trim()
+                ->value()
+        );
     }
 
     protected function age(): Attribute
