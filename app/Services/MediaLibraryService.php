b6f12bba2a7bc793545ff58e062aceee516d958e
Chinmay Purav <chinpurav@gmail.com>
2025-08-04 01:05:23 +0530

    loadTreePeopleImageUrl method added

--- app/Services/MediaLibraryService.php
+++ app/Services/MediaLibraryService.php
@@ -6,7 +6,10 @@ namespace App\Services;
 
 use App\Enums\MediaCollection;
 use App\Models\Person;
+use Illuminate\Support\Collection;
 use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use stdClass;
 
 class MediaLibraryService
 {
@@ -25,5 +28,23 @@ class MediaLibraryService
         }
 
         return count($uploads);
+    }
+
+    public function loadTreePeopleImageUrl(Collection $collection): Collection
+    {
+        $personIds = $collection->pluck('id')->toArray();
+
+        $photos = Media::where('model_type', Person::class)
+            ->whereIn('model_id', $personIds)
+            ->where('collection_name', MediaCollection::PHOTO->value)
+            ->where('order_column', 1)
+            ->get()
+            ->keyBy('model_id');
+
+        return $collection->each(function (stdClass $person) use ($photos) {
+            if ($photo = $photos->get($person->id)) {
+                $person->photo_url = $photo->getTemporaryUrl(now()->addHour());
+            }
+        });
     }
 }
