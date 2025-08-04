b6c57c7d1f1f42992714e98e47d61ef80054f441
Chinmay Purav <chinpurav@gmail.com>
2025-08-05 00:38:07 +0530

    Chore: update upcoming birthday test to include edge case for current year

--- tests/Feature/Services/PersonTest.php
+++ tests/Feature/Services/PersonTest.php
@@ -9,11 +9,12 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 uses(RefreshDatabase::class);
 
 test('it returns people with upcoming birthdays', function () {
-    $p1 = P::factory()->create(['dob' => now()->addMonths(3)]);
-    $p2 = P::factory()->create(['dob' => now()->addMonths(2)]);
-    $p3 = P::factory()->create(['dob' => now()->addMonths(1)]);
-    $p4 = P::factory()->create(['dob' => now()->addMonths(4)]);
+    $p1 = P::factory()->create(['dob' => now()->subYear()->addMonths(3)]);
+    $p2 = P::factory()->create(['dob' => now()->subYear()->addMonths(2)]);
+    $p3 = P::factory()->create(['dob' => now()->subYear()->addMonths(1)]);
+    $p4 = P::factory()->create(['dob' => now()->subYear()->addMonths(4)]);
+    $p5 = P::factory()->create(['dob' => today()->subYear()]);
 
-    expect($people = Person::upcomingBirthdayPeople(2))->toHaveCount(2);
-    expect($people->pluck('id')->toArray())->toBe([$p3->id, $p2->id]);
+    expect($people = Person::upcomingBirthdayPeople(2))->toHaveCount(3);
+    expect($people->pluck('id')->toArray())->toBe([$p5->id, $p3->id, $p2->id]);
 });
