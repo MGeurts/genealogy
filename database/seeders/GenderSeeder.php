606bf5d0c934d906003b68e1a51789ad6eeed7a2
Chinmay Purav <chinpurav@gmail.com>
2025-08-04 22:13:38 +0530

    chore: refactor GenderSeeder to use Eloquent for inserting gender records

--- database/seeders/GenderSeeder.php
+++ database/seeders/GenderSeeder.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Models\Gender;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 final class GenderSeeder extends Seeder
 {
@@ -14,64 +14,73 @@ final class GenderSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::Statement("
-            INSERT INTO `genders` (`name`) VALUES
-            ('Agender'),
-            ('Androgyne'),
-            ('Androgynous'),
-            ('Bigender'),
-            ('Cis'),
-            ('Cisgender'),
-            ('Cis Female'),
-            ('Cis Male'),
-            ('Cis Man'),
-            ('Cis Woman'),
-            ('Cisgender Female'),
-            ('Cisgender Male'),
-            ('Cisgender Man'),
-            ('Cisgender Woman'),
-            ('Female to Male'),
-            ('FTM'),
-            ('Gender Fluid'),
-            ('Gender Nonconforming'),
-            ('Gender Questioning'),
-            ('Gender Variant'),
-            ('Genderqueer'),
-            ('Intersex'),
-            ('Male to Female'),
-            ('MTF'),
-            ('Neither'),
-            ('Neutrois'),
-            ('Non-binary'),
-            ('Other'),
-            ('Pangender'),
-            ('Trans'),
-            ('Trans*'),
-            ('Trans Female'),
-            ('Trans* Female'),
-            ('Trans Male'),
-            ('Trans* Male'),
-            ('Trans Man'),
-            ('Trans* Man'),
-            ('Trans Person'),
-            ('Trans* Person'),
-            ('Trans Woman'),
-            ('Trans* Woman'),
-            ('Transfeminine'),
-            ('Transgender'),
-            ('Transgender Female'),
-            ('Transgender Male'),
-            ('Transgender Man'),
-            ('Transgender Person'),
-            ('Transgender Woman'),
-            ('Transmasculine'),
-            ('Transsexual'),
-            ('Transsexual Female'),
-            ('Transsexual Male'),
-            ('Transsexual Man'),
-            ('Transsexual Person'),
-            ('Transsexual Woman'),
-            ('Two-Spirit')
-        ");
+        $genders = [
+            'Agender',
+            'Androgyne',
+            'Androgynous',
+            'Bigender',
+            'Cis',
+            'Cisgender',
+            'Cis Female',
+            'Cis Male',
+            'Cis Man',
+            'Cis Woman',
+            'Cisgender Female',
+            'Cisgender Male',
+            'Cisgender Man',
+            'Cisgender Woman',
+            'Female to Male',
+            'FTM',
+            'Gender Fluid',
+            'Gender Nonconforming',
+            'Gender Questioning',
+            'Gender Variant',
+            'Genderqueer',
+            'Intersex',
+            'Male to Female',
+            'MTF',
+            'Neither',
+            'Neutrois',
+            'Non-binary',
+            'Other',
+            'Pangender',
+            'Trans',
+            'Trans*',
+            'Trans Female',
+            'Trans* Female',
+            'Trans Male',
+            'Trans* Male',
+            'Trans Man',
+            'Trans* Man',
+            'Trans Person',
+            'Trans* Person',
+            'Trans Woman',
+            'Trans* Woman',
+            'Transfeminine',
+            'Transgender',
+            'Transgender Female',
+            'Transgender Male',
+            'Transgender Man',
+            'Transgender Person',
+            'Transgender Woman',
+            'Transmasculine',
+            'Transsexual',
+            'Transsexual Female',
+            'Transsexual Male',
+            'Transsexual Man',
+            'Transsexual Person',
+            'Transsexual Woman',
+            'Two-Spirit',
+        ];
+
+        $now     = now();
+        $payload = collect($genders)
+            ->map(fn (string $name) => [
+                'name'       => $name,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ])->toArray();
+
+        Gender::insert($payload);
     }
 }
