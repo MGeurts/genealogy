3dc01cc1fd3d7fa1af8f40b9d05ae5d7239e60ec
MGeurts <5404867+MGeurts@users.noreply.github.com>
2025-08-12 21:32:20 +0200

    Disable activity logging for DeleteAccountTest

--- tests/Feature/Jetstream/DeleteAccountTest.php
+++ tests/Feature/Jetstream/DeleteAccountTest.php
@@ -6,17 +6,28 @@ use App\Models\User;
 use Laravel\Jetstream\Features;
 use Laravel\Jetstream\Http\Livewire\DeleteUserForm;
 use Livewire\Livewire;
+use Spatie\Activitylog\Models\Activity;
 
 uses(Illuminate\Foundation\Testing\RefreshDatabase::class);
+
+beforeEach(function (): void {
+    // Disable activity logging only for these tests
+    Activity::unsetEventDispatcher();
+});
+
+afterEach(function (): void {
+    // Restore activity logging after each test
+    Activity::setEventDispatcher(app('events'));
+});
 
 test('user accounts can be deleted', function (): void {
     if (! Features::hasAccountDeletionFeatures()) {
         $this->markTestSkipped('Account deletion is not enabled.');
     }
 
-    $this->actingAs($user = User::factory()->create());
+    $this->actingAs($user = User::factory()->withPersonalTeam()->create());
 
-    $component = Livewire::test(DeleteUserForm::class)
+    Livewire::test(DeleteUserForm::class)
         ->set('password', 'password')
         ->call('deleteUser');
 
@@ -30,7 +41,7 @@ test('correct password must be provided before account can be deleted', function
         $this->markTestSkipped('Account deletion is not enabled.');
     }
 
-    $this->actingAs($user = User::factory()->create());
+    $this->actingAs($user = User::factory()->withPersonalTeam()->create());
 
     Livewire::test(DeleteUserForm::class)
         ->set('password', 'wrong-password')
