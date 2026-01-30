7c8cbb6ffc540ce8ad9dedf156671e773efac849
Marc Geurts <webmaster@kreaweb.be>
2026-01-30 07:40:39 +0100

    Version bump

--- config/livewire.php
+++ config/livewire.php
@@ -71,7 +71,7 @@ return [
     */
 
     'make_command' => [
-        'type'  => 'sfc', // Options: 'sfc', 'mfc', 'class'
+        'type'  => 'mfc', // Options: 'sfc', 'mfc', 'class'
         'emoji' => true, // Options: true, false
         'with'  => [
             'js'   => false,
