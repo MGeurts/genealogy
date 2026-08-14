7a07b96650289fe2a959be28dd883c66683aaad9
KREAWEB.be <5404867+MGeurts@users.noreply.github.com>
2026-08-14 08:59:04 +0200

    Change filter condition to use str_starts_with

--- resources/views/components/tree-node/ancestors.blade.php
+++ resources/views/components/tree-node/ancestors.blade.php
@@ -6,7 +6,7 @@
     $person_sequence = $ancestors->firstWhere('id', $person->id)->sequence;
 
     $ancestors_next = $ancestors->where('degree', $level_current)->filter(function ($item) use ($person_sequence): bool {
-        return str_contains($item->sequence, $person_sequence.',');
+        return str_starts_with($item->sequence, $person_sequence.',');
     });
 @endphp
 
